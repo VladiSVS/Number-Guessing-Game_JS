4eb6e9837d6c01a5abf012ad8c802ace5f3ea982
VladiSVS <uladzimir.siauko@gmail.com>
2021-08-23 10:26:13 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let numInput = document.getElementById("numInput")
 let radioFour = document.getElementById("radioFour")
 let radioFive = document.getElementById("radioFive")
 let radioSix = document.getElementById("radioSix")
+let radios = document.getElementById("radios")
 let custom = document.getElementById("custom")
 let addText = document.getElementById("addText")
 let randomNum = Math.floor(Math.random() * 100) + 1
@@ -16,59 +17,39 @@ console.log(randomNum)
 getRechnen = () => {
     if (radioFour.checked) {
         attempts.innerHTML = radioFour.value
+        radios.style.display = "none"
         showCustomInput.style.display = "none"
         showAttempts.style.display = "block"
     } else if (radioFive.checked) {
         attempts.innerHTML = radioFive.value
+        radios.style.display = "none"
         showCustomInput.style.display = "none"
         showAttempts.style.display = "block"
     } else if (radioSix.checked) {
         attempts.innerHTML = radioSix.value
+        radios.style.display = "none"
         showCustomInput.style.display = "none"
         showAttempts.style.display = "block"
     } else if (custom.checked) {
         attempts.innerHTML = customNum.value
+        radios.style.display = "none"
         showCustomInput.style.display = "block"
         showAttempts.style.display = "block"
     }
 
     getBingo()
-    displayInfo()
     counter()
 
 }
 
 let index = 0
 counter = () => {
-    if (radioFour.checked && index >= radioFour.value) {
-        addText.innerHTML = `<p>Unlucky, attempts ended :(</p>`
-        return
-    } else if (radioFive.checked && index >= radioFive.value) {
-        addText.innerHTML = `<p>Unlucky, attempts ended :(</p>`
-        return
-    } else if (radioSix.checked && index >= radioSix.value) {
-        addText.innerHTML = `<p>Unlucky, attempts ended :(</p>`
-        return
-    } else if (custom.checked && index >= customNum.value) {
-        addText.innerHTML = `<p>Unlucky, attempts ended :(</p>`
+    if (radioFour.checked && index >= radioFour.value || radioFive.checked && index >= radioFive.value || radioSix.checked && index >= radioFive.value || custom.checked && index >= radioFive.value) {
+        addText.innerHTML = `<p>Unlucky, attempts ended :( <a href="index.html">Play Again</a></p></p>`
         return
     }
     index = index + 1
     attempt.innerHTML = index
-}
-
-getBingo = () => {
-    if (numInput.value == randomNum) {
-        addText.innerHTML = `<p>Yes!! You got me in ${index + 1}, i'm ${randomNum}. <a href="index.html">You win!! Play Again</a></p>`
-    }
-}
-
-displayInfo = () => {
-    if (numInput.value < randomNum) {
-        addText.innerHTML = `<p>${index + 1}- You need to guess higher than ${numInput.value}, try again...</p>`
-    } else if (numInput.value > randomNum) {
-        addText.innerHTML = `<p>${index + 1}- You need to guess lower than ${numInput.value}, try again...</p>`
-    }
 }
 
 attemptsFunc = () => {
@@ -95,3 +76,14 @@ attemptsFunc = () => {
         showAttempts.style.display = "none"
     }
 }
+
+getBingo = () => {
+    if (numInput.value == randomNum) {
+        addText.innerHTML = `<p>Yes!! You got me in ${index + 1}, i'm ${randomNum}. <a href="index.html">You win!! Play Again</a></p>`
+    } else if (numInput.value < randomNum) {
+        addText.innerHTML = `<p>${index + 1}- You need to guess higher than ${numInput.value}, try again...</p>`
+    } else if (numInput.value > randomNum) {
+        addText.innerHTML = `<p>${index + 1}- You need to guess lower than ${numInput.value}, try again...</p>`
+    }
+}
+
